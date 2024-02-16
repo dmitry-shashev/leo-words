5cd860b2adeec37c818d3d859364c8f8008e7524
Dmitry <old.reaver@gmail.com>
2024-02-16 13:58:08 +0200

    Update scripts

--- scripts/generate-words.ts
+++ scripts/generate-words.ts
@@ -14,38 +14,42 @@ if (!ACCOUNT_EMAIL || !ACCOUNT_PASSWORD) {
 }
 
 const WORDS_PATH_FULL = `${path.resolve('./')}/${WORDS_PATH}`
-const oldWords: Array<Word> = (await import(WORDS_PATH_FULL)).default
 
-const accessToken = await login(ACCOUNT_EMAIL, ACCOUNT_PASSWORD)
+// main
+;(async function (): Promise<void> {
+  const oldWords: Array<Word> = (await import(WORDS_PATH_FULL)).default
 
-// get all words
-const result: Array<Word> = []
-let lastWordInSet: number | null = null
-do {
-  const words = await getWords(accessToken, lastWordInSet)
-  result.push(...words)
+  const accessToken = await login(ACCOUNT_EMAIL, ACCOUNT_PASSWORD)
 
-  // eslint-disable-next-line no-console
-  console.log(
-    `Total: ${result.length}, found arr length: ${words.length}, last id: ${lastWordInSet}`
+  // get all words
+  const result: Array<Word> = []
+  let lastWordInSet: number | null = null
+  do {
+    const words = await getWords(accessToken, lastWordInSet)
+    result.push(...words)
+
+    // eslint-disable-next-line no-console
+    console.log(
+      `Total: ${result.length}, found arr length: ${words.length}, last id: ${lastWordInSet}`
+    )
+
+    lastWordInSet = words.at(-1)?.id ?? null
+  } while (lastWordInSet)
+
+  // try to apply images from old words
+  const wordsWithoutImg = result.filter(
+    (v) => !v.picture || v.picture.endsWith('f714.png')
   )
 
-  lastWordInSet = words.at(-1)?.id ?? null
-} while (lastWordInSet)
-
-// try to apply images from old words
-const wordsWithoutImg = result.filter(
-  (v) => !v.picture || v.picture.endsWith('f714.png')
-)
-
-for (let w of wordsWithoutImg) {
-  const oldImg = oldWords.find((old) => old.id === w.id)?.picture ?? ''
-  if (oldImg && !oldImg.endsWith('f714.png')) {
-    // @ts-ignore
-    w['picture'] = oldImg
-    // eslint-disable-next-line no-console
-    console.log(`Apply to ${w.wordValue}[${w.id}] image: ${oldImg}`)
+  for (let w of wordsWithoutImg) {
+    const oldImg = oldWords.find((old) => old.id === w.id)?.picture ?? ''
+    if (oldImg && !oldImg.endsWith('f714.png')) {
+      // @ts-ignore
+      w['picture'] = oldImg
+      // eslint-disable-next-line no-console
+      console.log(`Apply to ${w.wordValue}[${w.id}] image: ${oldImg}`)
+    }
   }
-}
 
-saveWords(result)
+  saveWords(result)
+})()
