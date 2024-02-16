f6a47bdfa9b72c7bae4717c88c93cd0171d0007d
Dmitry <old.reaver@gmail.com>
2024-02-16 13:37:37 +0200

    Update scripts and words

--- scripts/generate-extra-img.ts
+++ scripts/generate-extra-img.ts
@@ -15,29 +15,26 @@ async function findImg(search: string): Promise<string> {
   return img?.getAttribute('src')?.split('?')?.[0] ?? ''
 }
 
-// main
-;(async function (): Promise<void> {
-  const words: Array<Word> = (await import(WORDS_PATH_FULL)).default
+const words: Array<Word> = (await import(WORDS_PATH_FULL)).default
 
-  // mutate found words in order to add images
-  const wordsWithoutImg = words.filter(
-    (v) => !v.picture || v.picture.endsWith('f714.png')
-  )
+// mutate found words in order to add images
+const wordsWithoutImg = words.filter(
+  (v) => !v.picture || v.picture.endsWith('f714.png')
+)
 
-  for (let w of wordsWithoutImg) {
-    try {
-      await new Promise((r) => setTimeout(r, 1000))
-      const img = await findImg(w.wordValue)
-      // @ts-ignore
-      w['picture'] = img
+for (let w of wordsWithoutImg) {
+  try {
+    await new Promise((r) => setTimeout(r, 1000))
+    const img = await findImg(w.wordValue)
+    // @ts-ignore
+    w['picture'] = img
 
-      saveWords(words)
-      // eslint-disable-next-line no-console
-      console.log(`Apply to  ${w.wordValue}[${w.id}] image: ${img}`)
-    } catch (e) {
-      // @#$
-      // eslint-disable-next-line no-console
-      console.error(e)
-    }
+    saveWords(words)
+    // eslint-disable-next-line no-console
+    console.log(`Apply to  ${w.wordValue}[${w.id}] image: ${img}`)
+  } catch (e) {
+    // @#$
+    // eslint-disable-next-line no-console
+    console.error(e)
   }
-})()
+}
