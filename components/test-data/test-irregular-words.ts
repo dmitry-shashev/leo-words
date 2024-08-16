bbb8da2d77d6ca48956f0f3ce7e9cbc4350c1d07
Dmitry <old.reaver@gmail.com>
2024-08-16 13:34:13 +0300

    Test and words

---
+++ components/test-data/test-irregular-words.ts
@@ -0,0 +1,34 @@
+import { IrregularWord } from '@/models/irregular-word'
+
+export const testIrregularWords: Array<IrregularWord> = [
+  {
+    id: 1,
+    infinitive: 'bite',
+    pastSimple: 'bit',
+    pastParticiple: 'bitten',
+  },
+  {
+    id: 2,
+    infinitive: 'draw',
+    pastSimple: 'drew',
+    pastParticiple: 'drawn',
+  },
+  {
+    id: 3,
+    infinitive: 'do',
+    pastSimple: 'did',
+    pastParticiple: 'done',
+  },
+  {
+    id: 4,
+    infinitive: 'fly',
+    pastSimple: 'flew',
+    pastParticiple: 'flown',
+  },
+  {
+    id: 5,
+    infinitive: 'drive',
+    pastSimple: 'drove',
+    pastParticiple: 'driven',
+  },
+]
